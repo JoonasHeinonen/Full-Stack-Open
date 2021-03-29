8953c24cb1f8273bad166331d1310bd2b28e73d1
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2021-03-29 18:23:13 +0300

    Completed assignment 1.8.

--- part_one/unicafe/src/App.js
+++ part_one/unicafe/src/App.js
@@ -24,6 +24,36 @@ const PositiveProportion = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h2>Unicafe</h2>
+      <button onClick={() => props.setAllGoods(props.allGoods + 1)}>Good</button>
+      <button onClick={() => props.setAllNeutrals(props.allNeutrals + 1)}>Neutral</button>
+      <button onClick={() => props.setAllBads(props.allBads + 1)}>Bad</button>
+      <h2>Statistics</h2>
+      <p>Good: {props.allGoods.length}</p>
+      <p>Neutral: {props.allNeutrals.length}</p>
+      <p>Bad: {props.allBads.length}</p>
+      <All 
+        goods={props.allGoods.length} 
+        neutrals={props.allNeutrals.length} 
+        bads={props.allBads.length}
+      />
+      <Average 
+        goods={props.allGoods.length} 
+        neutrals={props.allNeutrals.length} 
+        bads={props.allBads.length}
+      />
+      <PositiveProportion 
+        goods={props.allGoods.length} 
+        neutrals={props.allNeutrals.length} 
+        bads={props.allBads.length}
+      />
+    </div>
+  )
+}
+
 const App = () => {
   const [allGoods, setAllGoods] = useState([])
   const [allNeutrals, setAllNeutrals] = useState([])
@@ -31,28 +61,13 @@ const App = () => {
 
   return (
     <div>
-      <h2>Unicafe</h2>
-      <button onClick={() => setAllGoods(allGoods + 1)}>Good</button>
-      <button onClick={() => setAllNeutrals(allNeutrals + 1)}>Neutral</button>
-      <button onClick={() => setAllBads(allBads + 1)}>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good: {allGoods.length}</p>
-      <p>Neutral: {allNeutrals.length}</p>
-      <p>Bad: {allBads.length}</p>
-      <All 
-        goods={allGoods.length} 
-        neutrals={allNeutrals.length} 
-        bads={allBads.length}
-      />
-      <Average 
-        goods={allGoods.length} 
-        neutrals={allNeutrals.length} 
-        bads={allBads.length}
-      />
-      <PositiveProportion 
-        goods={allGoods.length} 
-        neutrals={allNeutrals.length} 
-        bads={allBads.length}
+      <Statistics 
+        setAllGoods={setAllGoods}
+        setAllNeutrals={setAllNeutrals}
+        setAllBads={setAllBads}
+        allGoods={allGoods}
+        allNeutrals={allNeutrals}
+        allBads={allBads}
       />
     </div>
   )
