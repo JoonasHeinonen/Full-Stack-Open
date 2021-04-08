88b03eda1d35e18cac7cb415072c5c76396a0dc1
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2021-04-08 16:11:03 +0300

    Assignment 3.8 done.

--- part_three/phonedirectory/index.js
+++ part_three/phonedirectory/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 
+morgan.token('data', function (req, res) {
+    return JSON.stringify(req.body);
+});
+
 const PORT = 8000;
 const app = express();
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
 let persons = [
     {
