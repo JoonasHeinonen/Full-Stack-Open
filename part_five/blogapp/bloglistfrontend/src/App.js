99ae86f45abe956d16c6468abc633b54486e7337
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2021-05-03 19:36:17 +0300

    Renamed blogapp-frontend to blogappfrontend.

---
+++ part_five/blogapp/bloglistfrontend/src/App.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+
+const App = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        blogService.getAll().then(blogs =>
+            setBlogs( blogs )
+        );
+    }, []);
+
+    useEffect(() => {
+      const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+      if (loggedUserJSON) {
+          const user = JSON.parse(loggedUserJSON);
+          setUser(user);
+          blogService.setToken(user.token);
+      }
+    }, []);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const user = await loginService.login({
+            username, password
+        });
+
+        window.localStorage.setItem(
+            'loggedBlogappUser', JSON.stringify(user)
+        );
+
+        blogService.setToken(user.token);
+        setUser(user);
+        setUsername('');
+        setPassword('');
+    };
+
+    const handleLogout = async (e) => {
+        e.preventDefault();
+        console.log(window.localStorage);
+
+        window.localStorage.removeItem('loggedBlogappUser');
+        setUser(null);
+    };
+
+    const loginForm = () => {
+      return (
+          <div>
+                <h2>Login</h2>
+                <form onSubmit={handleLogin}>
+                    <div>
+                    username
+                        <input
+                            type='text'
+                            value={username}
+                            name='Username'
+                            onChange={({ target }) => setUsername(target.value)}
+                        />
+                    </div>
+                    <div>
+                    password
+                        <input
+                            type='password'
+                            value={password}
+                            name='Password'
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                    </div>
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
+        );
+    };
+
+    return (
+        <div>
+            <h1>Blog App</h1>
+            {user === null ?
+                loginForm() :
+                <div>
+                      <p>{user.name} logged in</p>
+                      <button onClick={handleLogout}>Logout</button>
+                      <h2>blogs</h2>
+                      {blogs.map(blog =>
+                        <Blog key={blog.id} blog={blog} />
+                      )}
+                </div>
+            }
+        </div>
+    );
+};
+
+export default App;
--- readme.md
+++ readme.md
@@ -55,4 +55,4 @@ The completed assignemnts can be reviewed directly from commit history on the ma
 
 * Part Four ( Osa 5, folder 'part_five'  ):
     * Part 5a ( Osa 5a ):
-        * Assignments 5.1 and 5.2. (Done. The folder for the project is in 'blogapp' with the foldername 'bloglist-frontend'.)
+        * Assignments s5.1 and 5.2. (Done. The folder for the project is in 'blogapp' with the foldername 'bloglist-frontend'.)
